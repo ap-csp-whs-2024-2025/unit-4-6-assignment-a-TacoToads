218615c33adf90199858bc2e60263a08e843c636
TacoToads <enhminecraft@gmail.com>
2025-01-14 21:47:52 +0000

    classwork

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,6 @@
 * contains the following procedures to work with lists:
 * display, append, insert, remove, length
 */
-#include "listhelpers.hpp"
 
 #include <cstdlib>   // srand, rand
 #include <ctime>     // time
@@ -18,15 +17,47 @@
 
 
 // TODO: Create squareNum
+void squareNum(int n)
+{
+    std::cout << "The square of " << n << " is " << (n*n) << std::endl;
 
+}
 // TODO: Create distance
+void distance( double x, double y)
+{
+    double dist = abs(x - y);
+    std::cout << "The distance between " << x << " and " << y << " is " << dist << std::endl;
+}
 
 // TODO: Create rollDice
+void rollDice()
+{
+    
+    int randomNumber = rand() % 6 + 1;
+    //print result
+    std::cout << "You rolled a " << randomNumber << std::endl;
+}
 
 int main()
 {
     // You can follow along with any code in class here
+    
+   //square nums 
+   // squareNum(5);
+   // squareNum(-3);
+   // squareNum(0);
 
+   //double num1;
+  // double num2;
+  // std::cout << "Enter two numbers\n";
+  // std::cin >> num1 >> num2;
+  // distance(num1, num2);
+
+    //seed the random number generator with current time
+   srand(time(0));
+   rollDice();
+   rollDice();
+   rollDice();
     
     return 0;
 }
